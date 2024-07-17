a76dfeca28d2c64a08b12ff662e9ef0f8c4eb772
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2024-07-17 22:24:56 +0200

    W, E and better (J K) that fix repeat rate

--- start.ps1
+++ start.ps1
@@ -1,3 +1,3 @@
-& C:\util\keymapper-4.4.4-win64\keymapper.exe -c .\keymapper.conf
+& C:\util\keymapper-4.4.4-win64\keymapper.exe -u -c .\keymapper.conf
 & C:\util\keymapper-4.4.4-win64\keymapperd.exe
-& python .\statuswindow.py C:\temp\keymapper.state.log
+& python .\statuswindow.py C:\temp\keymapper.state

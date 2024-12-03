d4d3b58b0614ad5dfc9b26733227f5e8e2499f2c
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2024-12-03 21:59:43 +0100

    version

--- start.ps1
+++ start.ps1
@@ -1,3 +1,3 @@
-& C:\util\keymapper-4.4.4-win64\keymapper.exe -u -c .\keymapper.conf
-& C:\util\keymapper-4.4.4-win64\keymapperd.exe
+& C:\util\keymapper-4.9.1-win64\keymapper.exe -u -c .\keymapper.conf
+& C:\util\keymapper-4.9.1-win64\keymapperd.exe
 & python .\statuswindow.py C:\temp\keymapper.state
